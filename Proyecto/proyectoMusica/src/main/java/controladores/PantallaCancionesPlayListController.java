dac3b8c843dd1f02a32fe1d8f2299caacdf51a31
IsraelOzuna <iro1904@hotmail.com>
2018-05-21 16:11:15 -0500

    Interfaces playlist e historial

---
+++ Proyecto/proyectoMusica/src/main/java/controladores/PantallaCancionesPlayListController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author iro19
+ */
+public class PantallaCancionesPlayListController implements Initializable {
+
+    @FXML
+    private Label lblPlaylist;
+    @FXML
+    private JFXButton btnDescargar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
